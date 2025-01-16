8e40ecacb3f27b04c1f02cf71cb2a5ae9ddfde6b
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2025-01-16 22:12:19 +0530

    Update appstore_data_panda_modification_problem.py

--- problemsolving/advanced/problems/appstore_data_panda_modification_problem.py
+++ problemsolving/advanced/problems/appstore_data_panda_modification_problem.py
@@ -1,13 +1,15 @@
 import pandas as pd
 from pathlib import Path
 
+
+
 base_path = Path(__file__).parent
 csv_directory_path = str((base_path / "../../../data/").resolve())
 df = pd.read_csv(csv_directory_path + '/googleplaystore.csv')
 # Check Data Types
 print(df.info())
-# perform data type conversion on 'Rating' and 'Reviews' column
-df['Reviews'].astype('float')
+
+# perform data type conversion on 'Reviews' column
 average_of_reviews = df['Reviews'].mean()
 print(f"The average number of reviews is: {average_of_reviews}")
 
